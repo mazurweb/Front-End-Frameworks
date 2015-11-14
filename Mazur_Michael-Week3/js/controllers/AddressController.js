a21d30fd3e6a612c67f1a6088cc9a4c666af7ade
Michael Mazur <mazurweb@gmail.com>
2015-11-13 20:19:10 -0500

    push

--- Mazur_Michael-Week3/js/controllers/AddressController.js
+++ Mazur_Michael-Week3/js/controllers/AddressController.js
@@ -2,22 +2,4 @@
  * Created by mikemaz2005 on 11/13/15.
  */
 
-app.controller('AddressController', function($scope, DataService) {
 
-    $scope.addresses = DataService.getAddresses();
-
-    $scope.newAddress = {};
-
-    $scope.addNewAddress= function() {
-        DataService.saveAddress($scope.newAddress.name, $scope.newAddress.street, $scope.newAddress.city, $scope.newAddress.state, $scope.newAddress.zipCode);
-        $scope.newAddress = {};
-    }
-
-    $scope.removeAddress = function(address) {
-        DataService.removeAddressAt(address);
-    }
-
-    $scope.clearIt = function(){
-        DataService.destroyLocalStorage();
-    }
-});
--- Mazur_Michael-Week3/js/list.js
+++ Mazur_Michael-Week3/js/list.js
@@ -2,7 +2,7 @@
  * Created by mikemaz2005 on 11/13/15.
  */
 
-var app = angular.module('myApp', ['ngRoute'])
+angular.module('myApp', ['ngRoute'])
 
     .config(function($routeProvider){
 
@@ -19,4 +19,25 @@ var app = angular.module('myApp', ['ngRoute'])
            redirectTo : "/grocery"
         });
 
+    })
+
+
+    .controller('AddressController', function($scope, DataService) {
+
+        $scope.addresses = DataService.getAddresses();
+
+        $scope.newAddress = {};
+
+        $scope.addNewAddress= function() {
+            DataService.saveAddress($scope.newAddress.name, $scope.newAddress.street, $scope.newAddress.city, $scope.newAddress.state, $scope.newAddress.zipCode);
+            $scope.newAddress = {};
+        }
+
+        $scope.removeAddress = function(address) {
+            DataService.removeAddressAt(address);
+        }
+
+        $scope.clearIt = function(){
+            DataService.destroyLocalStorage();
+        }
     });
